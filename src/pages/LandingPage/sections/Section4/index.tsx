dd66df81cefbf67498f184de81a2b6ce17ae699c
victorkossmann22 <victorkossmann22@gmail.com>
2024-08-01 13:34:08 +0000

    poggers

--- src/pages/LandingPage/sections/Section4/index.tsx
+++ src/pages/LandingPage/sections/Section4/index.tsx
@@ -1,6 +1,6 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.scss';
-import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Card } from '../../../../components/Card';
 import './styles.css';
 
