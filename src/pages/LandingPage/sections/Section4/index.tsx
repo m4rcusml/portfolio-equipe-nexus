57e1adafb710d2631fea13126a0f3555e0374f24
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2024-08-28 22:00:02 -0400

    Erro corrigido

--- src/pages/LandingPage/sections/Section4/index.tsx
+++ src/pages/LandingPage/sections/Section4/index.tsx
@@ -44,21 +44,6 @@ const data = [
 ]
 
 export function Section4() {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-
-export function Section4() {
     const [slidePercentage, setSlidePercentage] = useState(33);
     useEffect(() => {
         const updateSlidePercentage = () => {
